bf8d324a7dd34dcdb979a2773f83528e4139f103
Louis-David Perron <100434291+perronld@users.noreply.github.com>
2023-09-28 17:27:37 -0400

    Updated SASS script for new theme

--- update-wordpress.sh
+++ update-wordpress.sh
@@ -4,6 +4,7 @@ CLI_USER=root
 HTTP_HOST=dev-en.climatedata.ca
 
 ./compose-wrapper.sh exec -e HTTP_HOST="$HTTP_HOST" -e SERVER_PORT=443 -u $CLI_USER -w /var/www/html portal wp-cli search-replace dev-en.climatedata.ca climatedata.ca
+./compose-wrapper.sh exec -e HTTP_HOST="$HTTP_HOST" -e SERVER_PORT=443 -u $CLI_USER -w /var/www/html portal wp-cli search-replace dev-fr.climatedata.ca aPe2JnHUZbUy
 
 
 HTTP_HOST=climatedata.ca
@@ -11,5 +12,6 @@ HTTP_HOST=climatedata.ca
 ./compose-wrapper.sh exec -e HTTP_HOST="$HTTP_HOST" -e SERVER_PORT=443 -u $CLI_USER -w /var/www/html portal wp-cli core update 
 ./compose-wrapper.sh exec -e HTTP_HOST="$HTTP_HOST" -e SERVER_PORT=443 -u $CLI_USER -w /var/www/html portal wp-cli plugin upgrade --all
 ./compose-wrapper.sh exec -e HTTP_HOST="$HTTP_HOST" -e SERVER_PORT=443 -u $CLI_USER -w /var/www/html portal wp-cli search-replace climatedata.ca dev-en.climatedata.ca
+./compose-wrapper.sh exec -e HTTP_HOST="$HTTP_HOST" -e SERVER_PORT=443 -u $CLI_USER -w /var/www/html portal wp-cli search-replace aPe2JnHUZbUy dev-fr.climatedata.ca
 
 
